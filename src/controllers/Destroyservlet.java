371b72b29353e8c95faa81fc31d21c10e71a6983
mayumi.miyazaki <nmayuminn0628@gmail.con>
2020-06-24 00:41:45 +0900

    Add DestroyServlet.java

--- src/controllers/Destroyservlet.java
+++ src/controllers/Destroyservlet.java
@@ -16,13 +16,13 @@ import utils.DBUtil;
  * Servlet implementation class Destroyservlet
  */
 @WebServlet("/destroy")
-public class Destroyservlet extends HttpServlet {
+public class DestroyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Destroyservlet() {
+    public DestroyServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
