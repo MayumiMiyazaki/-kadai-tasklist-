02a648cfb147df6d032687a2726d4908448a03d6
mayumi.miyazaki <nmayuminn0628@gmail.con>
2020-06-23 23:10:23 +0900

    Add ShowServlet

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,12 +2,14 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import models.Task;
 import utils.DBUtil;
 
@@ -52,4 +54,4 @@ response.sendRedirect(request.getContextPath()+"/index");
 
     }
 
-}
+    }}
